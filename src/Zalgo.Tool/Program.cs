55347ac3b6144bbab980461abef7bf83c254552e
Mikhail Korolev <mkorolev@swiftcom.uk>
2019-10-01 12:19:20 +0300

    Change runner to global tool

---
+++ src/Zalgo.Tool/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+
+namespace Zalgo.Tool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            var text = args[0];
+
+            var res = new ZalgoString(text, FuckUpMode.Normal, FuckUpPosition.All);
+            TextCopy.Clipboard.SetText(res);
+            Console.WriteLine("Text copied to buffer");
+        }
+    }
+}
