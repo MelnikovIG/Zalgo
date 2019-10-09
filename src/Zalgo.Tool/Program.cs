36422023f912c17174dd4ccd9e71368dcb82f1c7
Michael Korolev <michael443959@gmail.com>
2019-10-09 20:28:00 +0300

    Add options support for tool

--- src/Zalgo.Tool/Program.cs
+++ src/Zalgo.Tool/Program.cs
@@ -5,12 +5,23 @@ namespace Zalgo.Tool
 {
     class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// The tool for generationg zalgo text
+        /// </summary>
+        /// <param name="input">Text for processing</param>
+        /// <param name="fuckUpMode">Fuck Up mode</param>
+        /// <param name="fuckUpPosition">Fuck Up position</param>
+        static void Main(string input, FuckUpMode fuckUpMode = FuckUpMode.Normal,
+            FuckUpPosition fuckUpPosition = FuckUpPosition.All)
         {
             Console.OutputEncoding = Encoding.UTF8;
-            var text = args[0];
+            if (string.IsNullOrEmpty(input))
+            {
+                Console.WriteLine("Input must not be null");
+                return;
+            }
 
-            var res = new ZalgoString(text, FuckUpMode.Normal, FuckUpPosition.All);
+            var res = new ZalgoString(input, fuckUpMode, fuckUpPosition);
             TextCopy.Clipboard.SetText(res);
             Console.WriteLine("Text copied to buffer");
         }
